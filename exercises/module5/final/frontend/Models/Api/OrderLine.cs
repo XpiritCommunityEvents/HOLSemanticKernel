43f16aa439118e1f559435df6fd8134b9f047121
Roy Cornelissen <ext_roy.cornelissen@jotron.com>
2025-11-09 15:27:03 +0100

    🔥 Remove final for now, will copy from finalized starter later

--- exercises/module5/final/frontend/Models/Api/OrderLine.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GloboTicket.Frontend.Models.Api;
-
-public class OrderLine
-{
-    public Guid EventId { get; set; }
-    public int TicketCount { get; set; }
-    public decimal Price { get; set; }
-
-}
--- exercises/module5/final/ordering/Model/OrderForCreation.cs
+++
@@ -1,8 +0,0 @@
-namespace GloboTicket.Ordering.Model;
-
-public class OrderForCreation
-{
-    public DateTimeOffset Date { get; set; }
-    public CustomerDetails CustomerDetails { get; set; }
-    public List<OrderLine> Lines { get; set; }
-}
